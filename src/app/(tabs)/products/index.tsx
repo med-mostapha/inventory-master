f6271b1875b9c23c2bf6cb064d0e5a62dd22ec1f
Med Elmostapha <dedijekani@gmail.com>
2025-12-12 18:03:46 +0000

    Prepare product list

--- src/app/(tabs)/products/index.tsx
+++ src/app/(tabs)/products/index.tsx
@@ -1,14 +1,24 @@
 import SearchBar from "@/src/components/ui/SearchBar";
+import { products } from "@/src/data/products";
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 
 export default function ProductScreen() {
   return (
-    <ScrollView className="p-2">
+    <View className="p-2">
       <View className="">
         <SearchBar placeholder="products..." onChange={() => {}} />
       </View>
       <Text className=" text-2xl font-bold">Product List</Text>
-    </ScrollView>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity className="">
+            <Text className="">{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
   );
 }
